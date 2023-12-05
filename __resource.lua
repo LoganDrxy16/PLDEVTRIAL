b1059e911a8d6c98a34d39116ec228e96eae6e73
LoganDrxy16 <121345094+LoganDrxy16@users.noreply.github.com>
2023-12-05 20:25:57 +0000

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,11 @@
+-- __resource.lua
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_script 'client.lua'
+ui_page 'index.html'
+
+files {
+    'style.css',
+    'index.html',
+}
